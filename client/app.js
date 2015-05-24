061f37c6de6f3f020df5e4b1e6443861250e8967
Ben Tranter <bwtrante@lakeheadu.ca>
2015-05-23 22:28:32 -0400

    Use DOMContentLoaded instead of domready

--- client/app.js
+++ client/app.js
@@ -1,19 +1,20 @@
 'use strict';
 
 // Module dependencies
-var DOMReady = require('domready');
 var MainView = require('./views/main');
 var Me       = require('./models/me');
 
 // The app's entry point. Register global event handlers, etc in here
 window.app = {
   init: function() {
+
+    // Initialize user model
     window.me = new Me();
-    var self = this;
-    // Wait for DOMReady before going wild
-    DOMReady(function() {
+
+    document.addEventListener('DOMContentLoaded', function() {
       self.view = new MainView({ el: document.body });
-    });
+    })
+    var self = this;
   }
 };
 
