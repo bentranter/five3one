3847098410c8cff93d211fba2d42ec2e4f63804e
Ben Tranter <bwtrante@lakeheadu.ca>
2015-05-26 14:52:35 -0400

    Add views

---
+++ client/views/new-workout-view.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  var newWorkoutView = Backbone.View.extend({
+
+  });
+})();
---
+++ client/views/timeline-view.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  var timelineView = Backbone.View.extend({
+
+  });
+})();
