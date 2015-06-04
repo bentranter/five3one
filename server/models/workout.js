403854cc42b9d0d0cde53be339803a8e0e4b201f
Ben Tranter <bwtrante@lakeheadu.ca>
2015-06-03 22:52:13 -0400

    Create workout model

---
+++ server/models/workout.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Module dependencies
+var thinky = require(__dirname + '/../util/thinky.js');
+
+// Keep reference to RethinkDB's utilities
+var r    = thinky.r;
+var type = thinky.type;
+
+var Workout = thinky.createModel('Workout', {
+  name: type.string(), // Useful for when we add ability to create any exercise
+  maxWeight: type.string(),
+  week: type.number(), // weeks cycle one through 14
+  createdAt: type.date().default(r.now()),
+  }, {
+  // Make the username the primary key
+  pk: 'username'
+});
+
+// Ensure index for ordering
+Workout.ensureIndex('createdAt');
+
+module.exports = Workout;
